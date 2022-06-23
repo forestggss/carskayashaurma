6fec986f3356620219ba2c4036cd341f327e72ac
forestggss <forestrnd161@gmail.com>
2022-06-23 23:17:37 +0300

    Актуальная

--- js/function.js
+++ js/function.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
         prevArrow: '<img class="intro__btn-prev" src="img/btn-prev.svg" alt="">',
         nextArrow: '<img class="intro__btn-next" src="img/btn-next.svg" alt="">',
     });
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 500,
+        prevArrow: '<img class="intro__btn-prev" src="img/btn-prev.svg" alt="">',
+        nextArrow: '<img class="intro__btn-next" src="img/btn-next.svg" alt="">',
+    });
   });
